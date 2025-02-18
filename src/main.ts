3854e9248b57bec2cdcb7787c1c3fb95534644f4
Denis Briceag <dbriceag@griddynamics.com>
2025-02-18 17:09:12 +0200

    Trigger pipeline

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? '3000');
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap().catch((err) => console.error(err));
